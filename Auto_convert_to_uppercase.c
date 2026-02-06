344951ca63c24f0c4e2c824b29016d3058d7910f
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 18:32:08 +0530

    Update Auto_convert_to_uppercase.c

--- Auto_convert_to_uppercase.c
+++ Auto_convert_to_uppercase.c
@@ -1,3 +1,21 @@
+// Write a C program that reads a single character from the user.
+
+// If the entered character is a lowercase alphabet (a to z), convert it into its uppercase equivalent and display it.
+
+// Otherwise, display the message "Already in upper case".
+
+// Input
+
+// A single character entered by the user.
+
+// Output
+
+// The uppercase version of the character if it is lowercase
+// OR
+
+// The message "Already in upper case" if the character is not lowercase.
+
+    
 #include<stdio.h>
 int main(){
     char ch;
