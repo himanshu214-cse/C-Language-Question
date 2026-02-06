e97fe1b50a2d168979d977dbfb3bfd35f633bbbc
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 18:55:41 +0530

    Update num_cube.c

--- num_cube.c
+++ num_cube.c
@@ -1,3 +1,36 @@
+// Write a C program to calculate the cube of a number.
+
+// The program should:
+
+// Ask the user to enter an integer number.
+
+// Calculate the cube of the number using the formula:
+
+// Cube=n×n×n
+
+// Display the result.
+
+// Input
+
+// An integer entered by the user.
+
+// Output
+
+// The cube of the entered number.
+
+// Example
+
+// Input
+
+// Enter a number = 3
+
+
+// Output
+
+// The cube of the number = 27
+    
+    
+    
 #include<stdio.h>
 int main() {
     int num;
