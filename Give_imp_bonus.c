11d042850ddda64eeb86f5f3b6f2636ffc8e46ea
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 14:38:12 +0530

    Create Give_imp_bonus.c

---
+++ Give_imp_bonus.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+    int year;
+    printf("Enter how old are you in this com : ");
+    scanf("%d",&year);
+    if(year>5){
+        printf("You are eli for bon..");
+    }
+    else{
+        printf("You are not ali for bon.");
+    }
+    return 0;
+}
