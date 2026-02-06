afa0f708362d4dad34b58d23581514a6406d8c31
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 19:34:11 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,45 @@
+// Write a C program to calculate the factorial of a given number using a for loop.
+
+// The program should:
+
+// Ask the user to enter an integer number N.
+
+// Calculate the factorial using the formula:
+
+// N!=1×2×3×⋯×N
+
+// Display the calculated factorial.
+
+// Input
+
+// An integer N entered by the user.
+
+// Output
+
+// The factorial of the number.
+
+// Example
+
+// Input
+
+// Enter a number: 5
+
+
+// Output
+
+// 120
+
+
+
+#include<stdio.h>
+int main() {
+    int num;
+    printf("Enetr a number: ");
+    scanf("%d", &num);
+    int fact = 1;
+    for(int i=1; i<=num; i++) {
+        fact = fact*i;
+    }
+    printf("%d", fact);
+    return 0;
+}
