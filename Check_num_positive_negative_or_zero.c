d984f8903ee425bba76eac05194181521f80910b
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 18:38:59 +0530

    Update Check_num_positive_negative_or_zero.c

--- Check_num_positive_negative_or_zero.c
+++ Check_num_positive_negative_or_zero.c
@@ -1,3 +1,57 @@
+// Write a C program to check whether a given number is positive, negative, or zero.
+
+// The program should:
+
+// Ask the user to enter an integer number.
+
+// Check the value of the number:
+
+// If the number is 0, display "The number is zero"
+
+// If the number is greater than 0, display "The number is positive"
+
+// If the number is less than 0, display "The number is negative"
+
+// Input
+
+// An integer entered by the user.
+
+// Output
+
+// A message indicating whether the number is positive, negative, or zero.
+
+// Example
+
+// Input
+
+// Enter the number = 5
+
+
+// Output
+
+// The number is positive
+
+
+// Input
+
+// Enter the number = -3
+
+
+// Output
+
+// The number is negative
+
+
+// Input
+
+// Enter the number = 0
+
+
+// Output
+
+// The number is zero
+
+
 #include<stdio.h>
 int main() {
     int n;
