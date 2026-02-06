1ed551cda49e9f2603fd93706bda120754ef561a
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 18:37:40 +0530

    Update Check_char_isupper_or_not.c

--- Check_char_isupper_or_not.c
+++ Check_char_isupper_or_not.c
@@ -1,3 +1,47 @@
+// Write a C program that checks whether a given character is uppercase or lowercase.
+
+// The program should:
+
+// Take a single character as input from the user.
+
+// Use the library function isupper() (from <ctype.h>) to check the case of the character.
+
+// Display:
+
+// "char is upper" if the character is uppercase
+
+// "char is lower" otherwise
+
+// Input
+
+// A single character entered by the user.
+
+// Output
+
+// A message indicating whether the character is uppercase or lowercase.
+
+// Example
+
+// Input
+
+// Enter any char : A
+
+
+// Output
+
+// char is upper
+
+
+// Input
+
+// Enter any char : b
+
+
+// Output
+
+// char is lower
+
+
 
 #include<stdio.h>
 #include<ctype.h>
