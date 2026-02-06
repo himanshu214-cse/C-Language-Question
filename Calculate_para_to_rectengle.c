8543909040337528e573b6f430cb1339b1009b86
Himanshu Verma <himanshu914095@gmail.com>
2026-02-06 18:35:04 +0530

    Update Calculate_para_to_rectengle.c

--- Calculate_para_to_rectengle.c
+++ Calculate_para_to_rectengle.c
@@ -1,3 +1,37 @@
+// Write a C program to calculate the perimeter of a rectangle.
+// The program should:
+
+// Ask the user to enter the length of the rectangle.
+
+// Ask the user to enter the width of the rectangle.
+
+// Calculate the perimeter using the formula:
+
+// Perimeter=2×(length+width)
+
+// Display the calculated perimeter.
+
+// Input
+
+// Two integers representing the length and width of the rectangle.
+
+// Output
+
+// The perimeter of the rectangle.
+
+// Example
+
+// Input
+
+// Enter the length = 5
+// Enter the width = 3
+
+
+// Output
+
+// The perimeter of the rectangle is = 16
+
+
 #include<stdio.h>
 int main() {
     int length;
