b121f69ed6db3c76f92a0c3ea82546b8ebc8e206
Himanshu Verma <hv914095@gmail.com>
2026-05-28 22:28:09 +0530

    Create fibonacci_using_recursion.c

---
+++ fibonacci_using_recursion.c
@@ -0,0 +1,29 @@
+Write a C program to find the nth Fibonacci number using recursion.
+
+
+#include <stdio.h>
+
+int fibo(int n);
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci number = %d", fibo(n));
+
+    return 0;
+}
+
+int fibo(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
